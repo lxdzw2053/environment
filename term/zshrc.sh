8fe35bbfac7bda1570e70acc2e9c4128f103070a
lx <88312178+lxdzw2053@users.noreply.github.com>
2023-01-05 20:12:27 -0800

    Update zshrc.sh

--- term/zshrc.sh
+++ term/zshrc.sh
@@ -1,5 +1,12 @@
+# for editing dotfiles
 alias vimrc="vim ~/.vimrc"
 alias zshrc="vim ~/.zshrc" 
+
+# project setup aliases 
+alias flask-setup="python3 -m venv venv; touch app.py; mkdir templates"
+alias build-system="touch Makefile; mkdir Docker; touch Docker/Dockerfile; touch .gitignore; git init;"
+
+# prompt code
 user_value=$(sed 's#.*/##' <<< $(pwd)) 
 
 if `git rev-parse --is-inside-work-tree 2>/dev/null`; then
