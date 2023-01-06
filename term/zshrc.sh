8e62523c1ec382f050a916d147b8498a5344ff82
lxdzw2053 <88312178+loadstringbyte@users.noreply.github.com>
2023-01-05 18:19:50 -0800

    Update zshrc.sh

--- term/zshrc.sh
+++ term/zshrc.sh
@@ -1,19 +1,20 @@
-user_value=$(sed 's#.*/##' <<< $(pwd))
+alias vimrc="vim ~/.vimrc"
+alias zshrc="vim ~/.zshrc" 
+user_value=$(sed 's#.*/##' <<< $(pwd)) 
 
 if `git rev-parse --is-inside-work-tree 2>/dev/null`; then
 	git_existence_status=" %F{magenta}git($(basename `git rev-parse --show-toplevel`))"
 else
 	git_existence_status=""
 fi
+  
 PS1="%F{cyan}zsh: ${user_value//$USER/~}$git_existence_status%F{cyan} $ %F{white}"
 
-
 foo () {
-  if [[ $PWD == $HOME ]]; then
-    source ~/.zshrc
-  else
-    source ~/.zshrc
-  fi
+	if [[ $PWD == $HOME ]]; then
+	     source ~/.zshrc
+	else
+	     source ~/.zshrc
+	fi
 }
-
 chpwd_functions+=(foo)
