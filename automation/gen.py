52c14356f9aa44783b2998c2c8dced899347e760
lxdzw2053 <88312178+lxdzw2053@users.noreply.github.com>
2023-01-16 17:39:39 -0800

    Update gen.py

--- automation/gen.py
+++ automation/gen.py
@@ -5,7 +5,7 @@ import random as r
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-project_ideas = ["operating system", "programming language", "browser", "container", "version control system", "text editor", "walking robot", "http router", "boring task automator (robot or code)", "game engine"]
+project_ideas = ["operating system", "programming language", "browser", "container", "version control system", "text editor", "walking robot", "http router", "boring task automator (robot or code)", "2d game engine"]
 
 def generate_repo():
 	# scope vars
