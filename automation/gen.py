0672c198d018b33332d94ceb4b76b128d2d1310a
lxdzw2053 <88312178+lxdzw2053@users.noreply.github.com>
2023-01-20 14:57:17 -0800

    Update gen.py

--- automation/gen.py
+++ automation/gen.py
@@ -4,7 +4,6 @@ import random as r
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
 project_ideas = ["operating system", "programming language", "browser", "container", "version control system", "text editor", "walking robot", "http router", "boring task automator (robot or code)", "2d game engine"]
 
 def generate_repo():
@@ -13,16 +12,12 @@ def generate_repo():
 	name = []
 	ret = {"project":"", "name":""}
 	
-	# generation
+	# logic
 	for i in range(len):
 		name.append(r.choice(letters))
 		name.append(r.choice(digits))
-
-	# redefinition
 	ret["name"] = str("".join(name))
 	ret["project"] = str(r.choice(project_ideas))
-	
-	# prints out the dictionary
 	print("-------------------------------------------")
 	print("PROJECT: {}".format(ret["project"]))
 	print("NAME:    {}".format(ret["name"]))
